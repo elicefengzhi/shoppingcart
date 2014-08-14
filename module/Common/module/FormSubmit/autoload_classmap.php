d812f75fa0c283668eb7f889361e4456faec07d1
elicefengzhi <elicefengzhi@gmail.com>
2014-08-14 10:25:33 +0800

    update FormSubmit autoload_classmap.php

--- module/Common/module/FormSubmit/autoload_classmap.php
+++ module/Common/module/FormSubmit/autoload_classmap.php
@@ -1,9 +1,18 @@
 <?php
 
 return array(
-    'FormSubmit\Module'                	   => __DIR__ . '/Module.php',
-	'FormSubmit\FormSubmit\BaseFormSubmit' => __DIR__ . '/src/FormSubmit/FormSubmit/BaseFormSubmit.php',
-	'FormSubmit\Logic\Insert'      		   => __DIR__ . '/src/FormSubmit/Logic/Insert.php',
-	'FormSubmit\Logic\Update'      		   => __DIR__ . '/src/FormSubmit/Logic/Update.php',
-	'FormSubmit\Service\FormSubmitFactory' => __DIR__ . '/src/FormSubmit/Service/FormSubmitFactory.php',
+    'FormSubmit\Module'                	         => __DIR__ . '/Module.php',
+	'FormSubmit\DbSql\DbSql'                     => __DIR__ . '/src/FormSubmit/DbSql/DbSql.php',
+	'FormSubmit\Exception\FormSubmitException'   => __DIR__ . '/src/FormSubmit/Exception/FormSubmitException.php',
+	'FormSubmit\Filter\Filter'                   => __DIR__ . '/src/FormSubmit/Filter/Filter.php',
+	'FormSubmit\Helper\BaseHelper'               => __DIR__ . '/src/FormSubmit/Helper/BaseHelper.php',
+	'FormSubmit\Helper\ChildColumns'             => __DIR__ . '/src/FormSubmit/Helper/ChildColumns.php',
+	'FormSubmit\Logic\Base'      		         => __DIR__ . '/src/FormSubmit/Logic/Base.php',
+	'FormSubmit\Logic\FormSubmit'      		     => __DIR__ . '/src/FormSubmit/Logic/FormSubmit.php',
+	'FormSubmit\Logic\Insert'      		         => __DIR__ . '/src/FormSubmit/Logic/Insert.php',
+	'FormSubmit\Logic\Update'      		         => __DIR__ . '/src/FormSubmit/Logic/Update.php',
+	'FormSubmit\Media\MediaUpload'      	     => __DIR__ . '/src/FormSubmit/Media/MediaUpload.php',
+	'FormSubmit\Service\FormSubmitFactory'       => __DIR__ . '/src/FormSubmit/Service/FormSubmitFactory.php',
+	'FormSubmit\Validate\OriginalParamsValidate' => __DIR__ . '/src/FormSubmit/Validate/OriginalParamsValidate',
+	'FormSubmit\Validate\Validate'               => __DIR__ . '/src/FormSubmit/Validate/Validate',
 );
